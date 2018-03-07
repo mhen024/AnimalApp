bda5bfca57a6ca49b1931d99547699544dba083c
mhen <mhen008@gmail.com>
2018-03-07 12:19:48 +0700

    Am a bird

--- app/src/main/java/sdu/cs58/suteenart/animalapp/GameActivity.java
+++ app/src/main/java/sdu/cs58/suteenart/animalapp/GameActivity.java
@@ -1,13 +1,70 @@
 package sdu.cs58.suteenart.animalapp;
 
+import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+
+import java.util.ArrayList;
+import java.util.Collections;
 
 public class GameActivity extends AppCompatActivity {
+
+    //ประกาศตัวแปร
+    Button btn1,btn2,btn3,btn4;
+    ImageView questionImageView;
+    ImageButton volumnImageButton;
+    MediaPlayer mediaPlayer; //เล่นไฟล์เสียง
+    int questionCount = 2;  //เก็บจำนวนคำถาม
+    ArrayList<Integer> qID = new ArrayList<Integer>(); //ชนิดของ Array ในการสุ่มคำถาม
+    String answer; //คำตอบ
+    int score = 0; //คะแนนจำค่าเป็น 0 ไว้ก่อน
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
+
+        //ผูก Element กับตัวแปรบน Java
+        btn1 = findViewById(R.id.btn1);
+        btn2 = findViewById(R.id.btn2);
+        btn3 = findViewById(R.id.btn3);
+        btn4 = findViewById(R.id.btn4);
+        questionImageView = findViewById(R.id.imvQuestion);
+        volumnImageButton = findViewById(R.id.imbVolumn);
+
+        //แสดงคำถาม
+        for (int i=1; i <= questionCount; i++) {
+            qID.add(i);
+        }
+        Collections.shuffle(qID); //กำหนดให้สุ่มคำถาม
+        setQuestion(qID.remove(0));
+
     }//End onCreate
+
+    private void setQuestion(int qID) { //ใช้กำหนดและเฉลยคำถามในแต่ละข้อ
+        if (qID == 1) {
+            answer = "นก";
+            questionImageView.setImageResource(R.drawable.bird);
+            mediaPlayer = MediaPlayer.create(this, R.raw.bird);
+            ArrayList<String> choice = new ArrayList<String>();
+            choice.add("นก");
+            choice.add("หมา");
+            choice.add("ช้าง");
+            choice.add("ยุง");
+            Collections.shuffle(choice);
+            btn1.setText(choice.remove(0));
+            btn2.setText(choice.remove(0));
+            btn3.setText(choice.remove(0));
+            btn4.setText(choice.remove(0));
+        }
+    }//End setQuestion
+
+    public void playSound(View view) {
+        mediaPlayer.start();
+    }//End playSound
+
 }//End Class
